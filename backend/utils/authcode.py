8da99536812884bec33abc8266f151dcd0943980
Alexey Shapiro <academ@gmail.com>
2024-02-24 19:10:33 +0200

    small code optimization - got rid of ._code variable in the AuthCode class, everything is stored in cache from now on

--- backend/utils/authcode.py
+++ backend/utils/authcode.py
@@ -18,30 +18,28 @@ class AuthCode:
 			raise ValidationError(f"{user} is not a User instance")
 		self._sender = None
 		self._user = user
-		self._code = None
 
 	def create_code(self) -> None:
-		self._generate_code()
-		self._store_code()
+		self._store_code(self._generate_code())
 		self._send_code()
 
 	def code_is_valid(self, code: int) -> bool:
-		return code == cache.get(self._user.email)
+		return code == self.code
 
 	def _generate_code(self) -> str:
-		self._code = str(random.randint(0, 9999)).zfill(4)
+		return str(random.randint(0, 9999)).zfill(4)
 
-	def _store_code(self) -> None:
-		cache.set(self._user.email, self._code, settings.ACCESS_RESTORE_CODE_TTL_SECONDS)
+	def _store_code(self, code: str) -> None:
+		cache.set(self._user.email, code, settings.ACCESS_RESTORE_CODE_TTL_SECONDS)
 
 	def _send_code(self) -> None:
 		if not self._sender:
 			raise ValidationError("Cannot send code: sender not set.")
-		self._sender.send_mail(self._user.email, self._code)
+		self._sender.send_mail(self._user.email, self.code)
 
 	def set_sender(self, sender: MailSender) -> None:
 		self._sender = sender
 
 	@property
 	def code(self):
-		return self._code
+		return cache.get(self._user.email)
