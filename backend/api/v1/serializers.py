f03b7fb5527a1382ee8db1a1cd8c690d635129ea
Alexey Shapiro <academ@gmail.com>
2024-02-25 08:06:02 +0200

    email already exists validation added upon registering + tests

--- backend/api/v1/serializers.py
+++ backend/api/v1/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework.validators import UniqueValidator
 
 User = get_user_model()
 
@@ -7,7 +8,7 @@ User = get_user_model()
 class CustomUserSerializer(serializers.ModelSerializer):
 	"""Serializer for the custom User model."""
 
-	email = serializers.EmailField()
+	email = serializers.EmailField(validators=(UniqueValidator(queryset=User.objects.all()),))
 
 	class Meta:
 		model = User
--- test/api_tests/user_tests.py
+++ test/api_tests/user_tests.py
@@ -1,4 +1,10 @@
+# TODO когда будет готов список обязательных полей для регистрации, сделать тесты на успех регистрации
+# TODO когда все обязательные поля заполнены, неуспех - если хотя бы одного из них нет, корректность
+# TODO заполнения модели данными из полей при регистрации.
+
+
 from rest_framework import status
+from rest_framework.test import APIClient
 from django.urls import reverse
 import pytest
 from django.contrib.auth import get_user_model
@@ -6,9 +12,32 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+def user_register_post_response(payload):
+    client = APIClient()
+    return client.post(reverse("user-register"), payload, format="json")
+
+
 @pytest.mark.django_db
-def test_user_is_registered(client):
+def test_user_is_registered():
     payload = {"email": "test@testuser.com"}
-    response = client.post(reverse("user-register"), payload, format="json")
+    response = user_register_post_response(payload)
     assert response.status_code == status.HTTP_201_CREATED
     assert User.objects.last().email == payload["email"]
+
+
+@pytest.mark.parametrize(
+    "incorrect_email",
+    ("test#testuser.com", "test@test@test.ru", "test.ru", "שלום@test.ru", "admin"),
+)
+@pytest.mark.django_db
+def test_incorrect_email_doesnt_pass_validation(incorrect_email):
+    response = user_register_post_response({"email": incorrect_email})
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+def test_cannot_register_if_a_user_already_exists(client):
+    payload = {"email": "test@testuser.com"}
+    User.objects.create(email=payload["email"])
+    response = user_register_post_response(payload)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
