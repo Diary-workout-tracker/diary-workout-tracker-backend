08c87433a839b0fc32870cc6d5de8179ea7d2cbf
Alexey Shapiro <academ@gmail.com>
2024-02-25 13:23:05 +0200

    changed view function to apiview class

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -1,5 +1,6 @@
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from .views import HealthCheckView, register_user
+from .views import HealthCheckView, RegisterUserView
+# , register_user
 
 from django.urls import path
 
@@ -8,5 +9,5 @@ urlpatterns = (
 	path("health/", HealthCheckView.as_view(), name="health"),
 	path("schema/", SpectacularAPIView.as_view(), name="schema"),
 	path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="docs"),
-	path("user/", register_user, name="user-register"),
+	path("user/", RegisterUserView.as_view(), name="user-register"),
 )
--- backend/api/v1/views.py
+++ backend/api/v1/views.py
@@ -2,12 +2,11 @@ from drf_spectacular.utils import extend_schema
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import permission_classes, api_view
+from rest_framework.decorators import permission_classes
 from rest_framework.permissions import AllowAny
 from .serializers import CustomUserSerializer
 from utils import authcode, mailsender
 
-# from djoser.views import UserViewSet
 from django.contrib.auth import get_user_model
 
 
@@ -32,17 +31,14 @@ class HealthCheckView(APIView):
 		return Response({"Health": "OK"})
 
 
-# class CustomUserViewSet(ModelViewSet):
-# 	queryset = User.objects.all()
-# 	serializer_class = CustomUserSerializer
-
-
-@api_view(["POST"])
 @permission_classes((AllowAny,))
-def register_user(request):
-	serializer = CustomUserSerializer(data=request.data)
-	if serializer.is_valid():
-		serializer.save()
-		send_auth_code(User.objects.get(email=request.data["email"]))
-		return Response(serializer.data, status=status.HTTP_201_CREATED)
-	return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class RegisterUserView(APIView):
+	serializer_class = CustomUserSerializer
+
+	def post(self, request, format=None):
+		serializer = self.serializer_class(data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			send_auth_code(User.objects.get(email=request.data["email"]))
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
