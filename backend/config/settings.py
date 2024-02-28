f946c9a6b1fa120cf1a9047006d3f47a2b280f08
Alexey Shapiro <academ@gmail.com>
2024-02-28 09:22:29 +0200

    poetry & pyproject conflicts

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -3,9 +3,12 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+path_to_env = os.path.join(BASE_DIR, "..", "infra", ".env")
+
+load_dotenv(path_to_env)
 
 SECRET_KEY = os.getenv("SECRET_KEY", default="secret_key")
 
@@ -68,6 +71,7 @@ DATABASES = {
 		"PASSWORD": os.getenv("POSTGRES_PASSWORD", default="password"),
 		"HOST": os.getenv("DB_HOST", default=""),
 		"PORT": os.getenv("DB_PORT", default=5432),
+		"PG_USER": os.getenv("PG_USER", default="user"),
 	}
 }
 
