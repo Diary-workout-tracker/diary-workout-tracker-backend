3baf03d0032f1ff55adac19d9b6612761867247f
Alexey Shapiro <academ@gmail.com>
2024-03-19 14:38:47 +0200

    2nd iteration after peer review

--- backend/utils/achievements.py
+++ backend/utils/achievements.py
@@ -1,5 +1,3 @@
-from django.db import transaction
-
 from running.models import Achievement, UserAchievement
 
 
@@ -14,12 +12,11 @@ def persistent(x):
 
 
 VALIDATORS = {
-	key.lower(): value
-	for key, value in {
-		"Экватор": equator,
-		"уПоРный": persistent,
-	}.items()
+	"Экватор": equator,
+	"уПоРный": persistent,
 }
+
+VALIDATORS = {key.lower(): value for key, value in VALIDATORS.items()}
 
 
 class AchievementUpdater:
@@ -27,7 +24,7 @@ class AchievementUpdater:
 	Проверяет по списку валидаторов для всех незавершенных ачивок,
 	если находит новую выполненную, добавляет в список. Обновляет базу.
 	Метод (свойство) new_achievements вызванный после update_achievements()
-	вернет список id новых, свежеполученных ачивок для дальнейшей десериализации.
+	вернет список свежеполученных ачивок для дальнейшей десериализации.
 	"""
 
 	def __init__(self, user, data) -> None:
@@ -46,23 +43,21 @@ class AchievementUpdater:
 		if not self._new_achievements:
 			return
 		user_achievements = [
-			UserAchievement(user_id=self._user, achievement_id=Achievement.objects.get(id=_id))
-			for _id in self._new_achievements
+			UserAchievement(user_id=self._user, achievement_id=achievement) for achievement in self._new_achievements
 		]
-		with transaction.atomic():
-			UserAchievement.objects.bulk_create(user_achievements)
+		UserAchievement.objects.bulk_create(user_achievements)
 
 	def _check_for_new_backend_achievements(self):
 		for achievement in self._unfinished_achievements:
 			validator = VALIDATORS.get(achievement.title.lower())
 			if validator is not None and validator(self._user) is True:
-				self._new_achievements.append(achievement.id)
+				self._new_achievements.append(achievement)
 
 	def _check_for_new_ios_achievements(self):
 		ios_achievements = self._data.get("achievements")
-		self._new_achievements.extend(
-			ios_achievements
-		)  # при условии, что по этому ключу будет список с id-шнниками новых иос-ачивок. Если будут названия, надо написать сравнение с названиями из query
+		if ios_achievements is not None:
+			# при условии, что по этому ключу будет список с id-шнниками новых иос-ачивок. Если будут названия, надо написать сравнение с названиями из query
+			self._new_achievements.extend(ios_achievements)
 
 	def _query_unfinished_achievements(self):
 		self._unfinished_achievements = Achievement.objects.exclude(
