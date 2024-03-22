a517252eaeeb28651e17d03c8ff9edb9d289bb32
Alexey Shapiro <academ@gmail.com>
2024-03-22 19:46:06 +0200

    logs - version 1.0

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -4,6 +4,9 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
+from .logs import LOGGING_SETTINGS
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 path_to_env = os.path.join(BASE_DIR, "..", "infra", ".env")
@@ -170,3 +173,6 @@ EMAIL_PORT = os.getenv("EMAIL_PORT", 465)
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "True")
+
+
+LOGGING = LOGGING_SETTINGS
